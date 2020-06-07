56184bbce255aa32ee8fbca8510134095c5e0545
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-07 15:05:31 -0300

    Created configurations for CRUD and Body Parser library added

--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/usuarios', function(req, res) {
+    res.json('get Usuario')
+});
+
+app.post('/usuarios', function(req, res) {
+    //Transforming information from request body with 
+    //"Body Parser" library
+    res.json({
+        person: req.body
+    })
+});
+
+app.put('/usuarios/:id', function(req, res) {
+
+    let id = req.params.id;
+    res.json({
+        id
+    })
+});
+
+app.delete('/usuarios', function(req, res) {
+    res.json('delete Usuario')
 });
 
 app.listen(3000, () => {
