32530717bad73ff44e20167e5a307b7f73423d00
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-13 20:27:24 -0300

    Change variable for MONGO URI

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDb;
 if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDb = 'mongodb+srv://admin:zRMaT5cF2E5PMmyK@cluster0-vuk71.mongodb.net/cooffe';
+    urlDb = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDb;
