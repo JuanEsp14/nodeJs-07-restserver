21f9680f7109ea1f977d621db3d33dee60e3e97a
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-13 20:08:45 -0300

    Difference of Databases choice for different environment

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,16 @@
 //Port for enviroment
 process.env.PORT = process.env.PORT || 3000;
+
+//Checking for enviroment
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//Selective database
+let urlDb;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDb = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDb = 'mongodb+srv://admin:zRMaT5cF2E5PMmyK@cluster0-vuk71.mongodb.net/cooffe';
+}
+
+process.env.URL_DB = urlDb;
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use(require('./routes/user'));
 
-mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true },
+mongoose.connect(process.env.URL_DB, { useNewUrlParser: true, useCreateIndex: true },
     (err, res) => {
         if (err) throw err;
         console.log("Base dedatos ONLINE");
