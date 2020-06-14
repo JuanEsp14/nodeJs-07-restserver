864b7312aff9162e1ab481723009fb006939e05f
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-14 15:19:43 -0300

    Login create

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,9 +4,40 @@ const bcrypt = require('bcrypt');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
-    })
+    let body = req.body;
+    User.findOne({ email: body.email }, (err, userDb) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!userDb) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: `The email or username ${body.email} do not exist`
+                }
+            });
+        }
+
+        if (!bcrypt.compareSync(body.password, userDb.password)) {
+            console.log("Your password incorrect");
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: `The email or username ${body.email} do not exist`
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            user: userDb,
+            token: '123'
+        })
+    });
 });
 
 module.exports = app;
