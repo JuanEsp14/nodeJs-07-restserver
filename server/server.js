cf5d19c67585b7c6e5fa401d7ef179b23454cf8f
Juan Espinoza <juan.espinoza@fluxit.com.ar>
2020-06-15 12:15:46 -0300

    Added google sign in

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config')
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 
@@ -10,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+//Enabled public folder
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 //Global config routes
 app.use(require('./routes/index'));
